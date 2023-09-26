f1c0de93d16035eeb2f1c97b16d07b4213ed80c9
Cyclonej <cyclonejmoto@gmail.com>
2023-09-26 08:09:05 +0200

    guestlist showing created names

--- src/pages/GuestList.jsx
+++ src/pages/GuestList.jsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import AddGuest from "../components/AddGuest";
+import Guestcard from "../components/Guestcard";
+
+const API_URL = "http://localhost:5005";
+
 function GuestList() {
+  const [guests, setGuests] = useState([]);
+
+  const getAllGuests = () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    axios
+      .get(`${API_URL}/api/guests`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => setGuests(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllGuests();
+  }, []);
+
   return (
     <div>
-      <h1>This is the GuestList Page</h1>
+
+      <div className="GuestList">
+        <AddGuest refreshGuests={getAllGuests} />
+
+        {guests.map((guest) => (
+          <Guestcard key={guest._id} {...guest} />
+        ))}
+      </div>
     </div>
   );
 }
