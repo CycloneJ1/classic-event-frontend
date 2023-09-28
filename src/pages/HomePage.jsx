a78088845e3f29cbd6483f6b36a73a085e854293
Cyclonej <cyclonejmoto@gmail.com>
2023-09-28 23:11:57 +0200

    Homepage layout changed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,13 +4,12 @@ import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 function HomePage() {
   return (
     <div>
-       <Container>
-          <h1>Welcome to Classic Event</h1>
-          <p>Your Premier Event Planning Partner</p>
-        </Container>
+      <Container className="mt-5">
+        <h1>Welcome to Classic Event</h1>
+        <p>Your Premier Event Planning Partner</p>
+      </Container>
 
-
-      <Container className="services" id="services">
+      <Container className="services mt-5" id="services">
         <h2>Services</h2>
         <Row>
           <Col>
@@ -21,18 +20,18 @@ function HomePage() {
         </Row>
       </Container>
 
-      <Container className="about" id="about">
+      <Container className="about mt-5" id="about">
         <h2>About Us</h2>
         <p>We are a dedicated team of event planners committed to making your dreams come true.</p>
       </Container>
 
-      <Container className="contact" id="contact">
+      <Container className="contact mt-5" id="contact">
         <h2>Contact Us</h2>
         <p>Have questions or ready to plan your event? Contact us today!</p>
-        <a href="mailto:contact@classicevent.com" className="contact-button">Email Us</a>
+        <a href="mailto:contact@classicevent.com" className="btn btn-primary">Email Us</a>
       </Container>
 
-      <footer className="footer">
+      <footer className="footer mt-5">
         <Container>
           <p>&copy; {new Date().getFullYear()} Classic Event. All rights reserved.</p>
         </Container>
