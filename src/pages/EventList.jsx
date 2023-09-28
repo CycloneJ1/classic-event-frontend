4bcd87e9d2fb6a961f6eb1a19707c999c4c191a7
Cyclonej <cyclonejmoto@gmail.com>
2023-09-28 11:53:08 +0200

    bug fix

--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -63,7 +63,7 @@ function EventList() {
       <h1>Events Created</h1>
       {events.map((event) => (
         <Card key={event._id}>
-          <Card.Img variant="top" src={event.imageUrl} alt={imageUrl} />
+          <Card.Img variant="top" src={event.imageUrl} alt={""} />
           <Card.Body>
             <Card.Title>{event.title}</Card.Title>
             <Card.Text>{event.description}</Card.Text>
